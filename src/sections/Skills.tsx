0878b47382f61f730c5b1681bbe3414eee2c93c9
Arfa Banyu Santoro <banyuarfa1122@gmail.com>
2025-04-21 20:32:30 +0700

    skills section progress part 1

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -3,13 +3,35 @@ import React from 'react';
 import { motion } from 'motion/react';
 
 const Skills = () => {
+  const words = 'skills skills skills skills skills skills skills'.split(' ');
   return (
     <SectionLayout>
-      <div>
-        <motion.div>tes</motion.div>
+      <div className='h-[500vh]'>
+        <motion.div className='h-screen grid place-content-center text-center'>
+          <h2 className='text-5xl font-mono leading-normal'>
+            a <span className='text-[#387478]'>MODERN WEB DEVELOPER</span>{' '}
+            focused on usability, functionality,{' '}
+            <span className='text-[#387478]'>SECURE SYSTEMS</span>, and seamless
+            interaction.
+          </h2>
+          <div className='text-7xl'>
+            <motion.p>
+              {words.map((word, i) => {
+                return <Word key={i}>{word}</Word>;
+              })}
+            </motion.p>
+            <motion.p>
+              {words.map((word, i) => {
+                return <Word key={i}>{word}</Word>;
+              })}
+            </motion.p>
+          </div>
+        </motion.div>
       </div>
     </SectionLayout>
   );
 };
-
+const Word = ({ children }) => {
+  return <span>{children}&nbsp;</span>;
+};
 export default Skills;
